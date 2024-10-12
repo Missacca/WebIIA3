012cd16159d1cbd136a7c26b9d13ad0554d67055
RealTakoChannel <873100637@qq.com>
2024-10-12 23:24:03 +0800

    change file structure

--- AdminSide/src/app/data.service.ts
+++ AdminSide/src/app/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Fundraiser} from "../models/fundraiser";
+import {Fundraiser} from "./models/fundraiser";
 
 @Injectable({
   providedIn: 'root'
