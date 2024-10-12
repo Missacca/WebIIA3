04e44c00c7452a87e9772b4c2bed279d488abb00
RealTakoChannel <873100637@qq.com>
2024-10-12 23:07:58 +0800

    Update api-controller.js

--- Server/controllerAPI/api-controller.js
+++ Server/controllerAPI/api-controller.js
@@ -166,7 +166,7 @@ router.get('/fundraiser/:id',  (req, res) => {
     const sql=` SELECT f.FUNDRAISER_ID, f.ORGANIZER, f.CAPTION, f.TARGET_FUNDING, f.CURRENT_FUNDING,f.CITY, f.ACTIVE, f.CATEGORY_ID, c.Category_Name, d.DONATION_ID, d.DATE, d.AMOUNT, d.GIVER FROM fundraiser f LEFT JOIN category c ON f.CATEGORY_ID = c.CATEGORY_ID  LEFT JOIN  donation d ON f.FUNDRAISER_ID = d.FUNDRAISER_ID  WHERE f.FUNDRAISER_ID = ?;`
     connection.execute(sql,[id],(err,records,fields) => {
         if (err) {
-            console.error('Error retrieving product:', err);
+            console.error('Error retrieving data:', err);
         } else {
             res.send(records);
         }
